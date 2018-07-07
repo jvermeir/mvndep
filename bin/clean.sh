d9ebd41f80dcd093e171a37459ac5b7a0bfc0de5
Jan Vermeir <jvermeir@hotmail.com>
2018-07-07 22:14:06 +0200

    branch names

--- bin/clean.sh
+++ bin/clean.sh
@@ -6,7 +6,7 @@ echo "branches: $BRANCHES"
 
 for BRANCH in "$BRANCHES"; do
     B=$(echo $BRANCH|xargs)
-    if [[ $B == 2018* ]]
+    if [[ $B == feature/mvnUpgrade* ]]
     then
         git branch -D $B
     fi
